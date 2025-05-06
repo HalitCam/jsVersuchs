088051c73ac862a0acb4361ce53d2456546b4e8e
Halit Cam <xltcamm@gmail.com>
2025-05-07 01:19:42 +0200

    Converttion

--- 04-veriTipi.js
+++ 04-veriTipi.js
@@ -9,5 +9,13 @@ console.log("number2: ", number2)," typeOfData :", typeof((number2))
 
 // number veri tipinden string e donusturmek
 let number5=55
-number5=number5.toString(number5)
+number5=number5.toString()
 console.log(number5, typeof(number5))
+
+//NAn = Not-A-Number //isNaN() YÖNTEMI ILE
+/* parseInt() String bir ifarde icerisindeki sayilari alararak number a dönüsturur. Ör: 11px -> 11 VE SADECE SAYI ILE BASLAMAK ZORUNDA!!!
+   number() sadece icerisinde sayi olan String ifadeleri number a dönüstürür. */
+
+/* parseFloat() */
+
+console.log(12/"5")
