6a72f82f9d650e6d2cb62ddd27385ecc241666d1
Halit Cam <xltcamm@gmail.com>
2025-05-07 02:33:56 +0200

    last changes added

--- template-literals.js
+++ template-literals.js
@@ -4,6 +4,12 @@ const DOMAIN = "trendstar.de"
 let email = username+ "@" + DOMAIN
 console.log("Merhaba", username, "sitemize hosgeldin", "mail aderesin", email )
 
-let info = ` Merhaba ${username
+let info = ` Merhaba ${username} sitemize hosgeldiniz.. mail adresin: ${email}
 
+kullanici mail adresinin uzunlugu : ${email.length}
+gunun saat bilgisi: ${new Date().getHours()}
+BORCUNUZ: ${(4+5)*3} TL
+`
+console.log(info)
 
+// Template Literals back tick kullanilarak yapilir ve ${} ifadeleri ile degiskenler einfach kullanilabilir
