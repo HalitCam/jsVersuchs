26293164e10287aef36629b7592c34cbc20ba99b
Halit Cam <xltcamm@gmail.com>
2025-05-09 00:26:48 +0200

    query selector was lernt

--- dom-icerisinden-element.js
+++ dom-icerisinden-element.js
@@ -1,3 +1,16 @@
-let title = document.getElementsByTagName("h1")
+// getElementById ile DOM elementlerine ulasilabilir
+// querySelector ile de ayni islem yapilabilinir. Birden fazla eleman olsada sadece bir tane secim yapar !!!
+
+
+
+let title = document.getElementById(`title`)
 console.log(title)
+title.innerHTML= "Degisen Bilgi"
 console.log(title.innerHTML)
+
+let link = document.querySelector("ul#list>li>a")
+console.log(link)
+console.log(link.innerHTML)
+link.innerHTML += " degisti"
+link.style.color = "red"
+link.classList.add("btn") 
