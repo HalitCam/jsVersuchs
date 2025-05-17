276435e7c9fac56ca7098e383c8f2fe84f05fb50
Halit Cam <xltcamm@gmail.com>
2025-05-17 02:48:12 +0200

    operates added

---
+++ karsilastirma-mantiksal-operatörler.js
@@ -0,0 +1,7 @@
+// *** Karsilastirma ve Mantiksal Operatorler***
+
+// Degeri Esitse ==
+console.log(100 == "100") // true dönecek
+
+// Hem degeri hem de türü esitse ===
+console.log(100 === "100") //false dönecek#
