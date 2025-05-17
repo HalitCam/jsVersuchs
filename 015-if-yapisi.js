6f5c8b9c59d784cf9f9e3220f65d81c289e631ce
Halit Cam <xltcamm@gmail.com>
2025-05-17 13:22:19 +0200

    if and switch structure added

---
+++ 015-if-yapisi.js
@@ -0,0 +1,20 @@
+let number = document.querySelector("#greeting")
+
+let getNumber = prompt("Write a number 1-100")
+
+if (getNumber%2 === 0) {
+    
+    if (getNumber =>2 && getNumber <= 5){
+    number.innerHTML = "Not Weird" }
+    else if (getNumber =>6 && getNumber <= 20){
+    number.innerHTML = "Weird"
+    }
+    else if (getNumber =>20 ){
+    number.innerHTML = "Not Weird"      
+    }
+    else number.innerHTML ="Weird" 
+
+}
+else number.innerHTML = "Weird"
+
+
---
+++ 016-switch-yapisi.js
@@ -0,0 +1,14 @@
+/* JavaScript'te if-else koşul yapısındaki koşulların arttığı durumlarda kod okunabilirliğini artırmak 
+ve daha performanslı sayılabilecek bir yapı olan SWITCH yapısı tercih edilebilir. */
+
+let trafik;
+trafik = "sikisik";
+switch(trafik){
+    case("sikisik"): console.log(" Disari Cikma")
+    break;
+    
+    case("hafif") : console.log( "Dikkatli ol")
+    
+    default: console.log ("Otur evinde")
+    
+}
