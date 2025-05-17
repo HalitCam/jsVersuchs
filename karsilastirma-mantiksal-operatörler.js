e95ef54160827066624bc4626da40eb819b224dc
Halit Cam <xltcamm@gmail.com>
2025-05-17 03:10:33 +0200

    last operates

--- karsilastirma-mantiksal-operatörler.js
+++ karsilastirma-mantiksal-operatörler.js
@@ -1,3 +1,6 @@
+let price= 100;
+let user = "hakan";
+
 // *** Karsilastirma ve Mantiksal Operatorler***
 
 // Degeri Esitse ==
@@ -5,3 +8,14 @@ console.log(100 == "100") // true dönecek
 
 // Hem degeri hem de türü esitse ===
 console.log(100 === "100") //false dönecek#
+
+// && ve operatörü
+console.log( price && user != "guest") // burada "price" bilgisi olsun VE user bilgisi esit olmasin "guest e diyor" // True döner
+
+// || veya operatorü
+console.log( price>10 || user != "guest") // burada "price" bilgisi olsun VE user bilgisi esit olmasin "guest e diyor" // True döner
+
+// ! degil (tersi) not operatörü
+console.log( price>10 && !user == "guest") 
+
+console.log(!!2)
