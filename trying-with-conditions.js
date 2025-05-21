46240e12bbad62d1ffd0fc2cbecc0de78a5b67b4
Halit Cam <xltcamm@gmail.com>
2025-05-21 23:12:19 +0200

    alle added

--- trying-with-conditions.js
+++ trying-with-conditions.js
@@ -2,40 +2,43 @@ let info1 = document.querySelector("h1#info1")
 info1.classList.add("blue")
 
 let note = Number((prompt("Please a lesoon note for evaluation 0-100")))
-inputEvelaute(note)
+
+
 
 function inputEvelaute(s) {
     switch (s) {
-        case s >= 90 && s < 100:
+        case (s >= 90 && s <= 100):
             return "AA"
 
-        case s >= 85 && s < 89:
+        case s >= 85 && s <= 89:
             return "BA"
 
-        case s >= 80 && s < 84:
+        case s >= 80 && s <= 84:
             return "BB"
 
-        case s >= 75 && s < 79:
+        case s >= 75 && s <= 79:
             return "CB"
 
-        case s >= 70 && s < 74:
+        case s >= 70 && s <= 74:
             return "CC"
 
-        case s >= 65 && s < 69:
+        case s >= 65 && s <= 69:
             return "DC"
 
-        case s >= 60 && s < 64:
+        case s >= 60 && s <= 64:
             return "DD"
 
-        case s >= 50 && s < 59:
+        case s >= 50 && s <= 59:
             return "FD"
 
-        case s >= 0 && s < 49:
+        case s >= 0 && s <= 49:
             return "AA"
-        default : inputEvelaute(prompt("Please again a Lesson Note 0-100 !!!")) 
+        default : "BASARISIZ"
         break
 
     
     }
 
 }
+
+console.log(inputEvelaute(note))
