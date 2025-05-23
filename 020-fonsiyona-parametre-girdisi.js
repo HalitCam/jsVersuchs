900b50124fd7d82e539704efe0e26c8dfac4a811
Halit Cam <xltcamm@gmail.com>
2025-05-23 23:05:24 +0200

    DOM eventList

--- 020-fonsiyona-parametre-girdisi.js
+++ 020-fonsiyona-parametre-girdisi.js
@@ -1,7 +1,16 @@
 // Fonk Parametre Almak ve Geri Dönüs Almak
 
-function greetings(firstname){
-    console.log(`Merhaba ${firstname ? firstname : " "}`)
+function greetings(firstname,lastname){
+    // console.log(`Merhaba ${firstname ? firstname : " "}`)
+    return `Merhaba ${firstname } ${lastname}`
 }
 
 greetings()
+
+function DOMideWrite(id,info){
+    let domObject = document.querySelector(`#${id}`)
+    domObject.innerHTML = info
+}
+
+let htmlInfo = `<span style= "color:red"> Color REDDD </span>`
+DOMideWrite("info2", htmlInfo)
