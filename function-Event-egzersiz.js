c7aca6899e8823c7a734701e85580197b6a4058f
Halit Cam <xltcamm@gmail.com>
2025-06-01 17:23:06 +0200

    Exercise to DomEvents

--- function-Event-egzersiz.js
+++ function-Event-egzersiz.js
@@ -1,25 +1,26 @@
-let counter = 0
-counter? counter :0
+localStorage.getItem("counter") ? counter = Number(localStorage.getItem("counter")) : counter = 0
+
+const decreaseDOM = document.querySelector( "#decrease")
+decreaseDOM.addEventListener("click", clickFunction)
+
+const increaseDOM = document.querySelector("#increase")
+increaseDOM.addEventListener("click", clickFunction)
+
+const counterDOM = document.querySelector ("#counter")
 
 
-function clickFunction(){
-    if (this == decreaseDOM){
-        counter -=1
-    } else if(this == increaseDOM){
-        counter ++ 
-
-    }
+function clickFunction() {
+    this.id == "increase"? counter ++ : counter --
+    counterDOM.innerHTML = counter
+    localStorage.setItem("counter",counter)
+    counter = Number(localStorage.getItem("counter"))
 }
 
 
-let counterDOM = document.querySelector("#counter")
-counterDOM.innerHTML = counter;
 
-let decreaseDOM = document.querySelector("decrease")
-decreaseDOM.addEventListener("click",clickFunction())
 
-let increaseDOM = document.querySelector("increase")
-increaseDOM.addEventListener("click",clickFunction())
+
+
 
 
 
