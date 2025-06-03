d99e68735536fc532eb187a10adb7636bd1859ca
Halit Cam <xltcamm@gmail.com>
2025-06-04 01:50:03 +0200

    JS First Projeckt

--- ilk-ödev.js
+++ ilk-ödev.js
@@ -1,20 +1,33 @@
 // Date Construction regulated
 const dateObj = new Date()
 const dayNumber = dateObj.getDay()
-let daysInfo = ["Monday" , "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]  
-const day = daysInfo[dayNumber-1]
+let daysInfo = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const day = daysInfo[dayNumber - 1]
 const hour = dateObj.getHours()
 const minute = dateObj.getMinutes()
 const seconds = dateObj.getSeconds()
 
-// last info is wrotten
-let info = `Hello, ${username} ! Welcome to our Website !
-${hour}:${minute}:${seconds}  ${day} 
-at this datum you are working about first Try
-`
 const formUsernameDOM = document.querySelector('#formUsername')
 formUsernameDOM.addEventListener("submit", submitFunction)
 
-function submitFunction(){
-    console.log("you clicked submit button")
+let username
+let usernameDOM = document.querySelector("#username")
+
+
+let helloinfoDOM = document.querySelector("#hello")
+
+function submitFunction(event) {
+    event.preventDefault()
+    username = usernameDOM.value
+    console.log(username)
+
+        // last info is wrotten
+    let info = `Hello, ${username} ! Welcome to our Website !
+    ${hour}:${minute}:${seconds}  ${day} 
+    at this date you are working about first try
+    `
+
+   helloinfoDOM.innerHTML = info
+
+
 }
