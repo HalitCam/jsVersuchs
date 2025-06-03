4b12242b8f8e72ee1dd5b459b3a821a538d8bc29
Halit Cam <xltcamm@gmail.com>
2025-06-03 23:49:23 +0200

    input parameter added by Form Structure

--- ilk-ödev.js
+++ ilk-ödev.js
@@ -1,20 +1,20 @@
+// Date Construction regulated
+const dateObj = new Date()
+const dayNumber = dateObj.getDay()
+let daysInfo = ["Monday" , "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]  
+const day = daysInfo[dayNumber-1]
+const hour = dateObj.getHours()
+const minute = dateObj.getMinutes()
+const seconds = dateObj.getSeconds()
 
+// last info is wrotten
+let info = `Hello, ${username} ! Welcome to our Website !
+${hour}:${minute}:${seconds}  ${day} 
+at this datum you are working about first Try
+`
+const formUsernameDOM = document.querySelector('#formUsername')
+formUsernameDOM.addEventListener("submit", submitFunction)
 
-let textDOM = document.querySelector("#text")
-
-
-let submitDOM = document.querySelector("#submit")
-submitDOM.addEventListener("click", clickFunction)
-
-let resetDOM = document.querySelector("#reset")
-resetDOM.addEventListener("click", clickFunction)
-
-function clickFunction() {
-    if(this.id == "submit"){
-        console.log(textDOM.innerHTML)
-    }
-
+function submitFunction(){
+    console.log("you clicked submit button")
 }
-
-
-
