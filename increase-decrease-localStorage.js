8161760fe9e610c0ff736f10dc697d39280a8e6d
Halit Cam <xltcamm@gmail.com>
2025-06-05 04:13:08 +0200

    Local Storage to counter was added

---
+++ increase-decrease-localStorage.js
@@ -0,0 +1,25 @@
+const monitorDOM = document.querySelector("#monitor")
+
+let counter = localStorage.getItem("counterInfo") ? Number(localStorage.getItem("counterInfo")) : 0 
+
+monitorDOM.innerHTML=counter
+
+const increaseDOM = document.querySelector("#increase")
+increaseDOM.addEventListener("click", clickFunction)
+
+const decreaseDOM = document.querySelector("#decrease")
+decreaseDOM.addEventListener("click", clickFunction)
+
+function clickFunction(){
+    this.id == "increase" ? counter ++ : counter --
+    monitorDOM.innerHTML = counter
+    localStorage.setItem("counterInfo", counter)
+    counter = localStorage.getItem("counterInfo")
+    
+ }
+
+
+//JSON ile Objekt yapilari ve karmasik yapilari JSON.stringify() ile string e ilk olarak cevirip LocalStorage da Strig
+//bir veri olarak tutup sonrasinda JSON.parse() islemi ile tekrardan istedigimiz halde veri olarak alabiliriz!!
+
+//localStorage alaninda kaydedilen bütün verileri localStorage.clear() ile sileriz !!
