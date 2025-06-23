60e16314fc68ab7f9afeb187efe1b4021a070b9d
Halit Cam <xltcamm@gmail.com>
2025-06-23 22:21:55 +0200

    try-catch added

--- 43-tryAndCatchUsage.js
+++ 43-tryAndCatchUsage.js
@@ -1,2 +1,22 @@
 // hata yönetimi icin try-catch kullanimi
+// try-catch ilebir sey oldu ise yap olmuyorsa hatayi göster diyerek kodun geri pkalan parcalarinin calismasi saglanir !!
+ let items; 
+try {
+    items.forEach(element => { //items datasi var ise calisacak yok ise hata gösterip devam edecek !!
+        console.log(element)
 
+    });
+} catch (error) {
+    console.log("hata: ", error)
+    items = [1,2,3,4,5,6]
+    items.forEach(item => console.log(item))
+}
+
+
+console.log("hata yönetimi düzgün calisiyor")
+
+let info = "kodluyoruz"
+console.log(info)
+
+// try-catch yapisi ile bilmedigimiz disardan alacagimiz bilgiler var ise bunlari bu yapinin
+//icerisine koyarak hata yönetimi yapariz !! 
